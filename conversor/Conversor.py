1dffe24f40a6087bd11585ffa1900558bde3291f
DamianHZ <hezedamian@hotmail.com>
2023-03-02 13:04:16 -0600

    interfaz

---
+++ conversor/Conversor.py
@@ -0,0 +1,22 @@
+#APP para convertir pies a metros usando tkinter
+#Herrera Zepeda Jose Damian
+#23/02/2023
+
+from tkinter import *
+from tkinter import ttk
+
+#Se crea clase
+class conversor:      
+
+#init funciona como constructor   #Raiz es la tabla
+
+    def __init__(self, raiz):
+
+        raiz.title("Pies a metros")  #titulo de la pagina
+      
+
+if __name__=="__main__":
+    print("este el archivo principal.")
+    print("Nada  mas se mostrara si este es el principal.")
+   
+
---
+++ conversor/Principal.py
@@ -0,0 +1,7 @@
+from tkinter import *
+from tkinter import Tk
+
+from Conversor import conversor
+raiz=Tk()
+conversor(raiz)
+raiz.mainloop()
